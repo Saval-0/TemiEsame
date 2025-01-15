0d6610407ddbe7715dafa5e78a71499941cc8463
Saval-0 <ivano.giordano242@gmail.com>
2025-01-15 16:20:06 +0100

    Done "database_canile"

--- esami2023-24/database_canile/main.py
+++ esami2023-24/database_canile/main.py
@@ -26,6 +26,61 @@
 # print(string.punctuation)
 ## ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
 
+def get_breed_level_avg(dogs, breed, level):
+    totScore = 0
+    count = 0
+    for key in dogs:
+        dog = dogs[key]
+        if dog[1] == breed and dog[2] == level:
+            totScore += float(dog[-1])
+            count += 1
 
-print(open('dogs.csv', 'r').read())
-print()
+    if count == 0:
+        return 0
+    return round(totScore / count, 2)
+
+# ['German Shepherd', 'Rottweiler', 'Poodle', 'Yorkshire Terrier', 'Golden Retriever', 'Beagle', 'Dachshund', 'Boxer', 'Bulldog', 'Labrador Retriever']
+# ['Intermediate', 'Beginner', 'Advanced', 'Expert']
+
+def main():
+    dogs = {}
+    breeds = []
+    levels = []
+
+    dogsFile = open('dogs.csv', 'r')
+    for i, line in enumerate(dogsFile):
+        if i == 0 or line == "":
+            continue
+        record = line.rstrip().split(",")
+        dogId = record[0]
+        if dogId not in dogs:
+            dogs[dogId] = []
+        
+        if record[2] not in breeds:
+            breeds.append(record[2])
+        if record[3] not in levels:
+            levels.append(record[3])
+        
+        for i, field in enumerate(record):
+            if i == 0:
+                continue
+            dogs[dogId].append(field)
+    dogsFile.close()
+
+    top = 0
+    topBreed = ""
+    for breed in breeds:
+        print("Razza: " + breed)
+        for level in levels:
+            avg = get_breed_level_avg(dogs, breed, level)
+            if level == "Expert" and avg > top:
+                topBreed = breed
+                top = avg
+            if avg != 0:
+                print(" * Livello " + level + ": media " + str(avg))
+
+    print("\n\nLa razza con il punteggio medio più alto per il livello Expert è " + topBreed  + ".")
+
+    return
+
+main()
